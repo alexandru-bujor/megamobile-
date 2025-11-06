0a8f87cc0c61ef0f53e234c2444c5ae951139141
Sandu Bujor <alexandru.bujor@isa.utm.md>
2025-11-07 00:37:23 +0200

    Build: docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+    base: mode === "production" ? "/megamobile-/" : "/", // project page path
+    server: { host: "::", port: 8080 },
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+    build: { outDir: "docs" } // <-- GitHub Pages will serve from /docs
 }));
